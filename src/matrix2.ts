77c908d415ea603dbe8979450c51ff348e59e2af
soloproyectos <gonzalo.chumillas@gmail.com>
2017-05-08 21:50:05 +0200

    recode

--- src/matrix2.ts
+++ src/matrix2.ts
@@ -1,152 +1,221 @@
 export class Point {
-  readonly x: number;
-  readonly y: number;
+  readonly z: Array<number>;
 
-  constructor (x: number, y: number) {
-    this.x = x;
-    this.y = y;
+  constructor (...z: Array<number>) {
+    this.z = z;
   }
 
   translate(vector: Vector): Point {
-    return new Point(this.x + vector.x, this.y + vector.y);
+    return new Point(...this.z.map(function (z: number, index: number) {
+      if (vector.z[index] === undefined) {
+        throw 'Argument error: invalid vector size';
+      }
+
+      return z + vector.z[index];
+    }));
+  }
+
+  toString(): string {
+    return `[${this.z.join(', ')}]`;
   }
 }
 
 export class Vector {
-  readonly x: number;
-  readonly y: number;
+  readonly z: Array<number>
 
-  constructor(x: number, y: number) {
-    [this.x, this.y] = [x, y];
+  constructor (...z: Array<number>) {
+    this.z = z;
   }
 
-  static createFromPoints(p0: Point, p1: Point): Vector {
-    let v0 = new Vector(p0.x, p0.y);
-    let v1 = new Vector(p1.x, p1.y);
-
-    return Vector.sub(v0, v1);
-  }
-
-  get opposite(): Vector {
-    return new Vector(-this.x, -this.y);
-  }
-
-  get length(): number {
-    return Math.sqrt(this.x * this.x + this.y * this.y);
-  }
-
-  // Multiplies the vector by a [value].
-  //
-  // Returns [this] * [value]
   scale(value: number): Vector {
-    return new Vector(value * this.x, value * this.y);
+    return new Vector(...this.z.map(function (z: number) {
+      return value * z;
+    }));
   }
 
-  // Multiplies [m] by the vector.
-  //
-  // Returns [m] * [this]
-  transform(m: SquareMatrix): Vector {
-    let [v0, v1] = m.vectors;
-
-    return new Vector(
-      v0.x * this.x + v1.x * this.y,
-      v0.y * this.x + v1.y * this.y
-    );
-  }
-
-  // Sum [v0] and [v1]
-  //
-  // Returns [v0] + [v1]
-  static sum(v0: Vector, v1: Vector): Vector {
-    return new Vector(v0.x + v1.x, v0.y + v1.y);
-  }
-
-  // Subtract [v1] from [v0];
-  //
-  // Returns [v0] - [v1]
-  static sub(v0: Vector, v1: Vector): Vector {
-    return Vector.sum(v1, v0.opposite);
+  toString(): string {
+    return `[${this.z.join(', ')}]`;
   }
 }
 
 export class Matrix {
-  readonly vectors: Array<Vector>;
+  readonly vectors: Array<Vector>
 
   constructor (...vectors: Array<Vector>) {
     this.vectors = vectors;
   }
 
-  // Scales a matrix
-  //
-  // Returns [this] * [value]
-  scale(value: number): Matrix {
-    let vectors = [];
+  scale(value: number) {
+    return new Matrix(...this.vectors.map(function (vector: Vector) {
+      return vector.scale(value);
+    }));
+  }
+}
 
-    for (let vector of this.vectors) {
-      vectors.push(vector.scale(value));
+export abstract class SquareMatrix extends Matrix {
+  constructor(...v: Array<Vector>) {
+    let size = v.length;
+    for (let vector of v) {
+      if (vector.z.length != size) {
+        throw 'Argument error: invalid square matrix';
+      }
     }
 
-    return new Matrix(...vectors);
+    super(...v);
   }
 }
 
-export class SquareMatrix extends Matrix {
+export namespace dim2 {
+  export class Point {
+    readonly x: number;
+    readonly y: number;
 
-  constructor (v0: Vector, v1: Vector) {
-    super(v0, v1);
+    constructor (x: number, y: number) {
+      this.x = x;
+      this.y = y;
+    }
+
+    translate(vector: Vector): Point {
+      return new Point(this.x + vector.x, this.y + vector.y);
+    }
   }
 
-  // Gets the matrix adjoint.
-  get adj(): SquareMatrix {
-    let [v0, v1] = this.vectors;
+  export class Vector {
+    readonly x: number;
+    readonly y: number;
 
-    return new SquareMatrix(new Vector(v1.y, -v0.y), new Vector(-v1.x, v0.x));
+    constructor(x: number, y: number) {
+      [this.x, this.y] = [x, y];
+    }
+
+    static createFromPoints(p0: Point, p1: Point): Vector {
+      let v0 = new Vector(p0.x, p0.y);
+      let v1 = new Vector(p1.x, p1.y);
+
+      return Vector.sub(v0, v1);
+    }
+
+    get opposite(): Vector {
+      return new Vector(-this.x, -this.y);
+    }
+
+    get length(): number {
+      return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    // Multiplies the vector by a [value].
+    //
+    // Returns [this] * [value]
+    scale(value: number): Vector {
+      return new Vector(value * this.x, value * this.y);
+    }
+
+    // Multiplies [m] by the vector.
+    //
+    // Returns [m] * [this]
+    transform(m: SquareMatrix): Vector {
+      let [v0, v1] = m.vectors;
+
+      return new Vector(
+        v0.x * this.x + v1.x * this.y,
+        v0.y * this.x + v1.y * this.y
+      );
+    }
+
+    // Sum [v0] and [v1]
+    //
+    // Returns [v0] + [v1]
+    static sum(v0: Vector, v1: Vector): Vector {
+      return new Vector(v0.x + v1.x, v0.y + v1.y);
+    }
+
+    // Subtract [v1] from [v0];
+    //
+    // Returns [v0] - [v1]
+    static sub(v0: Vector, v1: Vector): Vector {
+      return Vector.sum(v1, v0.opposite);
+    }
   }
 
-  // Gets the matrix determinant.
-  get det(): number {
-    let [v0, v1] = this.vectors;
+  export class Matrix {
+    readonly vectors: Array<Vector>;
 
-    return  v0.x * v1.y - v1.x * v0.y;
+    constructor (...vectors: Array<Vector>) {
+      this.vectors = vectors;
+    }
+
+    // Scales a matrix
+    //
+    // Returns [this] * [value]
+    scale(value: number): Matrix {
+      let vectors = [];
+
+      for (let vector of this.vectors) {
+        vectors.push(vector.scale(value));
+      }
+
+      return new Matrix(...vectors);
+    }
   }
 
-  // Gets the inverse of the matrix.
-  get inverse(): SquareMatrix {
-    return this.adj.scale(1 / this.det) as SquareMatrix;
-  }
-}
+  export class SquareMatrix extends Matrix {
 
-export class Line {
-  readonly point: Point;
-  readonly vector: Vector;
+    constructor (v0: Vector, v1: Vector) {
+      super(v0, v1);
+    }
 
-  constructor(point: Point, vector: Vector) {
-    this.point = point;
-    this.vector = vector;
-  }
+    // Gets the matrix adjoint.
+    get adj(): SquareMatrix {
+      let [v0, v1] = this.vectors;
 
-  // Gets the parallel line that contains the point [p].
-  getParallel(p: Point): Line {
-    return new Line(p, this.vector);
-  }
+      return new SquareMatrix(new Vector(v1.y, -v0.y), new Vector(-v1.x, v0.x));
+    }
 
-  // Gets the perpendicular line that contains the point [p].
-  getPerpendicular(p: Point): Line {
-    return new Line(p, new Vector(-this.vector.y, this.vector.x));
-  }
+    // Gets the matrix determinant.
+    get det(): number {
+      let [v0, v1] = this.vectors;
 
-  static isParallel(l0: Line, l1: Line): boolean {
-    let v0 = l0.vector;
-    let v1 = l1.vector;
+      return  v0.x * v1.y - v1.x * v0.y;
+    }
 
-    return v0.x * v1.y == v1.x * v0.y;
+    // Gets the inverse of the matrix.
+    get inverse(): SquareMatrix {
+      return this.adj.scale(1 / this.det) as SquareMatrix;
+    }
   }
 
-  static getIntersection(l0: Line, l1: Line): Point {
-    let m = new SquareMatrix(l0.vector, l1.vector.opposite);
-    let v = Vector.createFromPoints(l0.point, l1.point);
-    let w = v.transform(m.inverse);
+  export class Line {
+    readonly point: Point;
+    readonly vector: Vector;
 
-    return l1.point.translate(l1.vector.scale(w.y));
+    constructor(point: Point, vector: Vector) {
+      this.point = point;
+      this.vector = vector;
+    }
+
+    // Gets the parallel line that contains the point [p].
+    getParallel(p: Point): Line {
+      return new Line(p, this.vector);
+    }
+
+    // Gets the perpendicular line that contains the point [p].
+    getPerpendicular(p: Point): Line {
+      return new Line(p, new Vector(-this.vector.y, this.vector.x));
+    }
+
+    static isParallel(l0: Line, l1: Line): boolean {
+      let v0 = l0.vector;
+      let v1 = l1.vector;
+
+      return v0.x * v1.y == v1.x * v0.y;
+    }
+
+    static getIntersection(l0: Line, l1: Line): Point {
+      let m = new SquareMatrix(l0.vector, l1.vector.opposite);
+      let v = Vector.createFromPoints(l0.point, l1.point);
+      let w = v.transform(m.inverse);
+
+      return l1.point.translate(l1.vector.scale(w.y));
+    }
   }
 }
