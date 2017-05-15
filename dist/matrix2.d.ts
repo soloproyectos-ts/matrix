36a4407d65c156021652b398b712a6d6762db1e6
soloproyectos <gonzalo.chumillas@gmail.com>
2017-05-15 17:44:11 +0200

    update dist

--- dist/matrix2.d.ts
+++ dist/matrix2.d.ts
@@ -2,9 +2,8 @@ export declare class Vector {
     readonly w: Array<number>;
     constructor(...w: Array<number>);
     readonly length: number;
-    scale(value: number): Vector;
     readonly opposite: Vector;
-    transform(m: Matrix): Vector;
+    scale(value: number): Vector;
     static sum(v0: Vector, v1: Vector): Vector;
     static sub(v0: Vector, v1: Vector): Vector;
     toString(): string;
@@ -14,12 +13,11 @@ export declare class Matrix {
     constructor(...vectors: Array<Vector>);
     readonly width: number;
     readonly height: number;
-    getValue(col: number, row: number): number;
-    readonly isSquare: boolean;
     scale(value: number): Matrix;
-    getAdjoint(col: number, row: number): Matrix;
+    transpose(): Matrix;
     adjoint(): Matrix;
     determinant(): number;
     inverse(): Matrix;
     toString(): string;
+    private _getCofactor(col, row);
 }
--- dist/matrix2.js
+++ dist/matrix2.js
@@ -16,11 +16,6 @@ define(["require", "exports"], function (require, exports) {
             enumerable: true,
             configurable: true
         });
-        Vector.prototype.scale = function (value) {
-            return new (Vector.bind.apply(Vector, [void 0].concat(this.w.map(function (w) {
-                return value * w;
-            }))))();
-        };
         Object.defineProperty(Vector.prototype, "opposite", {
             get: function () {
                 return new (Vector.bind.apply(Vector, [void 0].concat(this.w.map(function (w) {
@@ -30,19 +25,10 @@ define(["require", "exports"], function (require, exports) {
             enumerable: true,
             configurable: true
         });
-        Vector.prototype.transform = function (m) {
-            var w = [];
-            if (m.width != this.length) {
-                throw 'invalid matrix width';
-            }
-            for (var i = 0; i < m.width; i++) {
-                var total = 0;
-                for (var j = 0; j < m.vectors.length; j++) {
-                    total += m.vectors[j].w[i] * this.w[j];
-                }
-                w.push(total);
-            }
-            return new (Vector.bind.apply(Vector, [void 0].concat(w)))();
+        Vector.prototype.scale = function (value) {
+            return new (Vector.bind.apply(Vector, [void 0].concat(this.w.map(function (w) {
+                return value * w;
+            }))))();
         };
         Vector.sum = function (v0, v1) {
             if (v0.length != v1.length) {
@@ -90,66 +76,61 @@ define(["require", "exports"], function (require, exports) {
             enumerable: true,
             configurable: true
         });
-        Matrix.prototype.getValue = function (col, row) {
-            return this.vectors[col].w[row];
-        };
-        Object.defineProperty(Matrix.prototype, "isSquare", {
-            get: function () {
-                return this.width == this.height;
-            },
-            enumerable: true,
-            configurable: true
-        });
         Matrix.prototype.scale = function (value) {
             return new (Matrix.bind.apply(Matrix, [void 0].concat(this.vectors.map(function (vector) {
                 return vector.scale(value);
             }))))();
         };
-        Matrix.prototype.getAdjoint = function (col, row) {
-            if (col > this.width - 1 || row > this.width - 1) {
-                throw 'Index out of bounds';
+        Matrix.prototype.transpose = function () {
+            var _this = this;
+            if (this.width != this.height) {
+                throw 'Not a square matrix';
             }
-            return new (Matrix.bind.apply(Matrix, [void 0].concat(this.vectors.filter(function (vector, index) {
+            return new (Matrix.bind.apply(Matrix, [void 0].concat(this.vectors.map(function (vector, col) {
+                return new (Vector.bind.apply(Vector, [void 0].concat(vector.w.map(function (number, row) { return _this.vectors[row].w[col]; }))))();
+            }))))();
+        };
+        Matrix.prototype.adjoint = function () {
+            var _this = this;
+            if (this.width != this.height) {
+                throw 'Not a square matrix';
+            }
+            return this.width == 1
+                ? new Matrix(new Vector(1))
+                : new (Matrix.bind.apply(Matrix, [void 0].concat(this.vectors.map(function (vector, col) {
+                    return new (Vector.bind.apply(Vector, [void 0].concat(vector.w.map(function (value, row) { return _this._getCofactor(col, row); }))))();
+                }))))().transpose();
+        };
+        Matrix.prototype.determinant = function () {
+            var _this = this;
+            var vector = this.width > 0 ? this.vectors[0] : new Vector();
+            var initVal = this.width > 0 ? 0 : 1;
+            if (this.width != this.height) {
+                throw 'Not a square matrix';
+            }
+            return vector.w.reduce(function (prev, current, index) { return prev + current * _this._getCofactor(0, index); }, initVal);
+        };
+        Matrix.prototype.inverse = function () {
+            if (this.width != this.height) {
+                throw 'Not a square matrix';
+            }
+            return this.adjoint().scale(1 / this.determinant());
+        };
+        Matrix.prototype.toString = function () {
+            return this.vectors.map(function (vector) {
+                return vector.toString();
+            }).join('\n');
+        };
+        Matrix.prototype._getCofactor = function (col, row) {
+            var sign = (col + row) % 2 > 0 ? -1 : +1;
+            var m = new (Matrix.bind.apply(Matrix, [void 0].concat(this.vectors.filter(function (vector, index) {
                 return index != col;
             }).map(function (vector, index) {
                 return new (Vector.bind.apply(Vector, [void 0].concat(vector.w.filter(function (value, index) {
                     return index != row;
                 }))))();
             }))))();
-        };
-        Matrix.prototype.adjoint = function () {
-            if (!this.isSquare) {
-                throw 'Not a square matrix';
-            }
-            return null;
-        };
-        Matrix.prototype.determinant = function () {
-            var self = this;
-            var ret = 0;
-            if (!this.isSquare) {
-                throw 'Not a square matrix';
-            }
-            if (this.width > 0) {
-                var vector = this.vectors[0];
-                var initVal = this.width < 2 ? vector.w[0] : 0;
-                ret = vector.w.reduce(function (prev, current, index) {
-                    var sign = index % 2 > 0 ? -1 : +1;
-                    var adj = self.getAdjoint(0, index);
-                    return prev + sign * current * adj.determinant();
-                }, initVal);
-            }
-            return ret;
-        };
-        Matrix.prototype.inverse = function () {
-            if (!this.isSquare) {
-                throw 'Not a square matrix';
-            }
-            return null;
-        };
-        Matrix.prototype.toString = function () {
-            return this.vectors.map(function (vector) {
-                return vector.toString();
-            }).join('\n');
+            return sign * m.determinant();
         };
         return Matrix;
     }());
