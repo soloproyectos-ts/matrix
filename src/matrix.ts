b0b22cc7fe48e5515a129f795c751bd58159c1ae
soloproyectos <gonzalo.chumillas@gmail.com>
2017-08-11 21:29:16 +0200

    todo

--- src/matrix.ts
+++ src/matrix.ts
@@ -7,6 +7,7 @@ export interface Transformable {
   transform(t: Transformation): Transformable;
 }
 
+// TODO: remove this class
 export class Point implements Positionable, Transformable {
   readonly coordinates: number[];
 
