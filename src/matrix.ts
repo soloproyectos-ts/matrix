73184baaa9a64200b4b74580c39b7990cfc1f14c
soloproyectos <gonzalo.chumillas@gmail.com>
2017-09-14 00:27:46 +0200

    replace tabs by spaces

--- src/matrix.ts
+++ src/matrix.ts
@@ -235,10 +235,10 @@ export class Transformation extends SquareMatrix implements Transformable {
 
 // Converts [angle] to degrees.
 export function rad2deg(angle: number): number {
-	return 180 * angle / Math.PI;
+  return 180 * angle / Math.PI;
 }
 
 // Converts [angle] to radians.
 export function deg2rad(angle: number): number {
-	return Math.PI * angle / 180;
+  return Math.PI * angle / 180;
 }
